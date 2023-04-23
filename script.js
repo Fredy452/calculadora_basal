eb10616a964619e1facce1076db594aab4f77dad
Fred452 <fredyarce452@gmail.com>
2023-04-23 12:17:05 -0400

    calculadora terminada

--- script.js
+++ script.js
@@ -5,32 +5,47 @@ const CALCULAR = document.getElementById('calcular');
 const ERROR = document.getElementById('error');
 const FLU = document.getElementById('flu');
 const MAN = document.getElementById('man');
+const TITULO = document.getElementById('detalle-titulo');
+const LISTA = document.getElementById('detalle-lista');
+
+
+
 
 CALCULAR.addEventListener('click', () =>{
-    const PESO = document.getElementById('peso').value;
-    // Validación del input de peso
-    if(PESO > 0){
+    const PESO = document.getElementById('peso').value;  
+    // Validamos si el imput esta vacio o el numero es negativo
+    if (PESO === "") {
+        ERROR.innerHTML = '*Por favor ingrese el peso'
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    } else if (PESO >= 0 && PESO <= 30) {
         ERROR.style.display = 'none'
         let flujo = calculoFlujo(PESO);
         let mantenimiento = flujo * 1.5;
-        FLU.innerHTML = flujo + 'cc/hr';
+        FLU.innerHTML = flujo + ' cc/hr';
         MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
         FLU.style.display = 'block';
         MAN.style.display = 'block';
+        TITULO.innerHTML = 'Calculo por Holliday-Segar';
+        LISTA.innerHTML = '<li>De 0kg a 10kg, se calcula 100cc por cada kilo.</li> <li>Se suman 50cc por cada kilo de peso por arriba de 10kg, hasta 20kg.</li> <li>De 20kg para arriba, se suman 20cc por cada kilo adicional</li> ';
+    }else if (PESO <= 100) {
+         // Hacemos el calculo por superficie corporal
+         let valores = superficieCorporal(PESO);
+         FLU.innerHTML = valores[0] +' cc/24h' + ' *1500';
+         MAN.innerHTML = valores[1] +' cc/24h' + ' *2000';
+         ERROR.style.display = 'none'
+         FLU.style.display = 'block';
+         MAN.style.display = 'block';
+         TITULO.innerHTML = 'Calculo por Superficie Corporal';
+         LISTA.innerHTML = 'Cuando el niño tiene más de 30kg: Se calcula la hidratación basal usando el método de superficie corporal.';
+
     }else{
-        ERROR.style.display = 'block'
+        ERROR.innerHTML = '*Peso permitido hasta los 100 kg'
+        ERROR.style.display = 'block';
         FLU.style.display = 'none';
         MAN.style.display = 'none';
-    }if(PESO > 30){
-        // Hacemos el calculo por superficie corporal
-        let valores = superficieCorporal(PESO);
-        FLU.innerHTML = valores[0] +' cc/24h' + ' *1500';
-        MAN.innerHTML = valores[1] +' cc/24h' + ' *2000';
-        ERROR.style.display = 'none'
-        FLU.style.display = 'block';
-        MAN.style.display = 'block';
     }
-    // Se calcula el peso flujo
     function calculoFlujo(peso){
             let flujo = 0;
             if(peso <= 30){
@@ -39,27 +54,20 @@ CALCULAR.addEventListener('click', () =>{
                     let rest = peso - 20;
                     flujo = ((10 * 100) + (10 * 50) + (rest * 20)) / 24;
                     return Math.round(flujo);
-                }
-
-                if(peso <= 20 && peso > 10){
+                } else if(peso <= 20 && peso > 10){
                     let rest = peso - 10;
                     flujo = ((10 * 100) + (rest * 50)) / 24;
                     return Math.round(flujo);   
-                }
-
-                if(peso <= 10){
+                } else if(peso <= 10){
                     flujo = (peso * 100) / 24;
                     return Math.round(flujo);
                 }
-            }
-            else
-            {
+            } else {
                 ERROR.style.display = 'block';
                 FLU.style.display = 'none';
                 MAN.style.display = 'none';
             }
     }
-
     function superficieCorporal(PESO){
         // Aprendiendo por las malas a parcear un valor
         let peso = parseFloat(PESO);
